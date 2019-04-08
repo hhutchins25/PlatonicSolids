74123eee2f8012152b242bb3df66b252382fa4b8
holden <hhutchins25@gmail.com>
2019-04-08 16:00:22 -0700

    Initial commit

---
+++ app/src/main/java/com/example/platonicsolids/GLActivity.java
@@ -0,0 +1,86 @@
+package com.example.platonicsolids;
+
+import android.content.Context;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.platonicsolids.geo.Triangle;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class GLActivity extends AppCompatActivity {
+
+    public static class MyGLRenderer implements GLSurfaceView.Renderer {
+
+        private float r = 0.25f;
+        private float g = 0.5f;
+        private float b = 1.0f;
+
+        private Triangle mTriangle;
+
+        public void onSurfaceCreated(GL10 unused, EGLConfig config) {
+            // Set the background frame color
+            GLES20.glClearColor(r, g, b, 1.0f);
+            // initialize a triangle
+            mTriangle = new Triangle();
+        }
+
+        public void onDrawFrame(GL10 unused) {
+            // Redraw background color
+            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+            r += r < 1.0f ? 0.001f : -0.4f;
+            g += g < 1.0f ? 0.002f : -0.8f;
+            b += b < 1.0f ? 0.005f : -1.0f;
+            GLES20.glClearColor(r, g, b, 1.0f);
+            mTriangle.draw();
+        }
+
+        public void onSurfaceChanged(GL10 unused, int width, int height) {
+            GLES20.glViewport(0, 0, width, height);
+        }
+
+        public static int loadShader(int type, String shaderCode){
+
+            // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+            // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+            int shader = GLES20.glCreateShader(type);
+
+            // add the source code to the shader and compile it
+            GLES20.glShaderSource(shader, shaderCode);
+            GLES20.glCompileShader(shader);
+
+            return shader;
+        }
+    }
+
+    class MyGLSurfaceView extends GLSurfaceView {
+
+        private final MyGLRenderer renderer;
+
+        public MyGLSurfaceView(Context context){
+            super(context);
+
+            // Create an OpenGL ES 2.0 context
+            setEGLContextClientVersion(2);
+
+            renderer = new MyGLRenderer();
+
+            // Set the Renderer for drawing on the GLSurfaceView
+            setRenderer(renderer);
+        }
+    }
+
+    private GLSurfaceView gLView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity.
+        gLView = new MyGLSurfaceView(this);
+        setContentView(gLView);
+    }
+}
---
+++ app/src/main/java/com/example/platonicsolids/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.platonicsolids;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        // TextView tv = findViewById(R.id.sample_text);
+        // tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    // public native String stringFromJNI();
+
+    public void openRenderer(View view) {
+        Intent intent = new Intent(this, GLActivity.class);
+        startActivity(intent);
+    }
+}
