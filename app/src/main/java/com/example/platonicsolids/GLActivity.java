0dd9170a47a794dd850df06a64abc158faa858ae
holden <hhutchins25@gmail.com>
2019-04-08 19:31:11 -0700

    2 - Projection, simple motion controls applied
    - projection is now applied correctly view, adjusts upon any resolution change
    - triangle rotates along a single axis with touch controls

--- app/src/main/java/com/example/platonicsolids/GLActivity.java
+++ app/src/main/java/com/example/platonicsolids/GLActivity.java
@@ -8,8 +8,11 @@ package com.example.platonicsolids;
 import android.content.Context;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
-import android.support.v7.app.AppCompatActivity;
+import android.opengl.Matrix;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+
 import com.example.platonicsolids.geo.Triangle;
 
 import javax.microedition.khronos.egl.EGLConfig;
@@ -23,7 +26,23 @@ public class GLActivity extends AppCompatActivity {
         private float g = 0.5f;
         private float b = 1.0f;
 
+        // vPMatrix is an abbreviation for "Model View Projection Matrix"
+        private final float[] vPMatrix = new float[16];
+        private final float[] projectionMatrix = new float[16];
+        private final float[] viewMatrix = new float[16];
+
+        private float[] rotationMatrix = new float[16];
         private Triangle mTriangle;
+
+        public volatile float mAngle;
+
+        public float getAngle() {
+            return mAngle;
+        }
+
+        public void setAngle(float angle) {
+            mAngle = angle;
+        }
 
         public void onSurfaceCreated(GL10 unused, EGLConfig config) {
             // Set the background frame color
@@ -39,17 +58,39 @@ public class GLActivity extends AppCompatActivity {
             g += g < 1.0f ? 0.002f : -0.8f;
             b += b < 1.0f ? 0.005f : -1.0f;
             GLES20.glClearColor(r, g, b, 1.0f);
-            mTriangle.draw();
+
+            // Set the camera position (View matrix)
+            Matrix.setLookAtM(viewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+
+            // Calculate the projection and view transformation
+            Matrix.multiplyMM(vPMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
+
+            float[] scratch = new float[16];
+
+            // Create a rotation for the triangle
+            Matrix.setRotateM(rotationMatrix, 0, mAngle, 0, 0, -1.0f);
+
+            // Combine the rotation matrix with the projection and camera view
+            // Note that the vPMatrix factor *must be first* in order
+            // for the matrix multiplication product to be correct.
+            Matrix.multiplyMM(scratch, 0, vPMatrix, 0, rotationMatrix, 0);
+
+            // Draw triangle
+            mTriangle.draw(scratch);
         }
 
         public void onSurfaceChanged(GL10 unused, int width, int height) {
             GLES20.glViewport(0, 0, width, height);
+
+            float ratio = (float) width / height;
+
+            // this projection matrix is applied to object coordinates
+            // in the onDrawFrame() method
+            Matrix.frustumM(projectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
         }
 
         public static int loadShader(int type, String shaderCode){
 
-            // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
-            // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
             int shader = GLES20.glCreateShader(type);
 
             // add the source code to the shader and compile it
@@ -64,6 +105,46 @@ public class GLActivity extends AppCompatActivity {
 
         private final MyGLRenderer renderer;
 
+        private final float TOUCH_SCALE_FACTOR = 180.0f / 320;
+        private float previousX;
+        private float previousY;
+
+        @Override
+        public boolean onTouchEvent(MotionEvent e) {
+            // MotionEvent reports input details from the touch screen
+            // and other input controls. In this case, you are only
+            // interested in events where the touch position changed.
+
+            float x = e.getX();
+            float y = e.getY();
+
+            switch (e.getAction()) {
+                case MotionEvent.ACTION_MOVE:
+
+                    float dx = x - previousX;
+                    float dy = y - previousY;
+
+                    // reverse direction of rotation above the mid-line
+                    if (y > getHeight() / 2) {
+                        dx = dx * -1 ;
+                    }
+
+                    // reverse direction of rotation to left of the mid-line
+                    if (x < getWidth() / 2) {
+                        dy = dy * -1 ;
+                    }
+
+                    renderer.setAngle(
+                            renderer.getAngle() +
+                                    ((dx + dy) * TOUCH_SCALE_FACTOR));
+                    requestRender();
+            }
+
+            previousX = x;
+            previousY = y;
+            return true;
+        }
+
         public MyGLSurfaceView(Context context){
             super(context);
 
@@ -74,6 +155,9 @@ public class GLActivity extends AppCompatActivity {
 
             // Set the Renderer for drawing on the GLSurfaceView
             setRenderer(renderer);
+
+            // Render the view only when there is a change in the drawing data
+            setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
         }
     }
 
